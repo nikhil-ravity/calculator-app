c27c8157675e08b78352eb4ff85725f4a07c2d99
nikhil-ravity <nikhilworkmail0@gmail.com>
2025-08-29 14:55:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000'
+  origin: process.env.FRONTEND_URL || 'http://40.81.242.220:80'
 }));
 app.use(express.json());
 
@@ -38,5 +38,6 @@ app.use('*', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
 
 module.exports = app;
