b62320ab09b5f08752c457a81c5f94fa46779e72
Ubuntu <ubuntu@machine-1.uhd5n0cuzlbevn24i1hnvmiw0b.rx.internal.cloudapp.net>
2025-09-18 08:40:29 +0000

    Revert "updated files from azure"
    This reverts commit c575fb06aed80ac4d9361e6e81964a1a58ab2d0e.

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-	origin: process.env.FRONTEND_URL || 'http://4.187.185.255/'
+  origin: process.env.FRONTEND_URL || 'http://40.81.242.220'
 }));
 app.use(express.json());
 
